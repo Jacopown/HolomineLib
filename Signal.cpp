4bcdec2dacd3df75058bbbcca33d9c151c421be6
jacopown <jacopown@macbookpro>
2025-12-04 15:33:51 +0100

    working Signal implementation

--- Signal.cpp
+++ Signal.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
@@ -18,40 +17,52 @@ Signal::Signal(const string& filename) {
       throw invalid_argument("failed to open file: " + filename);
   }
   string line;
+
+  //["DATE", "FREQUENCY_TX", "FREQUENCY_RX", "MOD_I", "PHASE_I", "MOD_Q", "PHASE_Q", "MOD", "PHASE"];
+  if (getline(file, line))
+
   while (getline(file, line)) {
       stringstream ss(line);
       string cell;
       datapoint datapoint;
 
-      getline(ss, cell, ';');
-      datapoint.timestamp = stol(cell);
+      getline(ss, cell, ' ');
+      std::tm t = {};
+      std::istringstream date_ss(cell);
+      
+      date_ss >> std::get_time(&t, "%Y/%m/%d-%H:%M:%S"); 
+      
+      char dot;
+      int micros = 0;
+      date_ss >> dot >> micros;
 
-      getline(ss, cell, ';');
-      datapoint.x = stod(cell)/1000;
+      time_t utc_seconds = timegm(&t);
 
-      getline(ss, cell, ';');
-      datapoint.y = stod(cell)/1000;
+      datapoint.timestamp = (static_cast<long long>(utc_seconds) * 1000000) + micros;
 
-      getline(ss, cell, ';');
-      datapoint.z = stod(cell)/1000;
+      getline(ss, cell, ' ');
+      datapoint.frequency = stod(cell);
+
+      for(int i=0; i<5; ++i) {
+        getline(ss, cell, ' ');
+      }
+
+      getline(ss, cell, ' ');
+      double Mod = stod(cell);
+
+      getline(ss, cell, ' ');
+      double Phase = stod(cell);
+
+      datapoint.I = Mod*sin(Phase);
+      datapoint.Q = Mod*cos(Phase);
 
       data.push_back(datapoint);
-
+      setSize(getSize() + 1);
   }
-
-  double H = data[2500].z;
-  data.erase(remove_if(data.begin(), data.end(), 
-        [H](const datapoint& p) {
-            return fabs(p.z - H) > 1.0;
-            }),
-    data.end()
-  );
-
-  size = data.size();
 };
 
-void Position::print(size_t startingIndex, size_t lastIndex) const {
+void Signal::print(size_t startingIndex, size_t lastIndex) const {
   for (size_t i = startingIndex; i < lastIndex; i++) {
-    cout << data[i].timestamp << " " << data[i].x << " " << data[i].y << " " << data[i].z << "\n"; 
+    cout << data[i].timestamp << " " << data[i].frequency << " " << data[i].I << " " << data[i].Q << "\n"; 
   }
 }
