4c189b2c6d8d50ad6fb111ea14f55e51ccb86689
jacopown <jacopomehi@gmail.com>
2025-12-13 18:37:00 +0100

    usefull main and little tweaks to make everything work as expected

--- src/Hologram.cpp
+++ src/Hologram.cpp
@@ -5,17 +5,21 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-Hologram::Hologram(const SignalLog& signalLog, int xStep, int yStep) {
+Hologram::Hologram(const SignalLog &signalLog, int xStep, int yStep) {
 
-  width = static_cast<int>(std::ceil((signalLog.getWidth() - signalLog.getMinX()) / xStep)) + 1;
-  height = static_cast<int>(std::ceil((signalLog.getHeight() - signalLog.getMinY()) / yStep)) + 1;
-  std::cout << "width " << signalLog.getWidth();
-  std::cout << "width " << signalLog.getHeight();
-  hologram2d.resize(width*height);
-  size = width*height;
-  std::vector<double> count(width*height);
+  width = static_cast<int>(
+              std::ceil((signalLog.getWidth() - signalLog.getMinX()) / xStep)) +
+          1;
+  height = static_cast<int>(std::ceil(
+               (signalLog.getHeight() - signalLog.getMinY()) / yStep)) +
+           1;
+  std::cout << "Width " << signalLog.getWidth() << "\n";
+  std::cout << "Height " << signalLog.getHeight() << "\n";
+  hologram2d.resize(width * height);
+  size = width * height;
+  std::vector<double> count(width * height);
 
-  std::vector<signalDatapoint> data = signalLog.getData();  
+  std::vector<signalDatapoint> data = signalLog.getData();
   double minX = signalLog.getMinX();
   double minY = signalLog.getMinY();
 
@@ -26,34 +30,34 @@ Hologram::Hologram(const SignalLog& signalLog, int xStep, int yStep) {
     double Phase = data[i].Phase;
     double Re = Mod * cos(Phase);
     double Im = Mod * sin(Phase);
-    int col = static_cast<int>((x - minX)/xStep);
-    int row = static_cast<int>((y - minY)/yStep);
-    int index = row*width+col;
+    int col = static_cast<int>((x - minX) / xStep);
+    int row = static_cast<int>((y - minY) / yStep);
+    int index = row * width + col;
     hologram2d[index] += std::complex<double>(Re, Im);
     count[index]++;
   }
 
-  for (size_t i = 0; i < size ; i++) {
+  for (size_t i = 0; i < size; i++) {
     hologram2d[i] = hologram2d[i] / count[i];
   }
 }
 
-void Hologram::saveImage(const std::string& path, bool real){
-  cv::Mat complex_mat(height, width, CV_64FC2, hologram2d.data());  
+void Hologram::saveImage(const std::string &path, bool real) {
+  cv::Mat complex_mat(height, width, CV_64FC2, hologram2d.data());
   std::vector<cv::Mat> channels;
   cv::split(complex_mat, channels);
 
   cv::Mat visual;
-  real? cv::normalize(channels[0], visual, 0, 255, cv::NORM_MINMAX, CV_8UC1): cv::normalize(channels[1], visual, 0, 255, cv::NORM_MINMAX, CV_8UC1);
-  
+  real ? cv::normalize(channels[0], visual, 0, 255, cv::NORM_MINMAX, CV_8UC1)
+       : cv::normalize(channels[1], visual, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+
   cv::Mat color_image;
   cv::applyColorMap(visual, color_image, cv::COLORMAP_JET);
 
   bool success = cv::imwrite(path, color_image);
 
-  success? std::cout << "Successfully saved Re image to: " << path << std::endl : std::cerr << "Failed to save image to: " << path << std::endl;
+  if (!success)
+    std::cerr << "Failed to save image to: " << path << std::endl;
 }
 
-void Hologram::print(size_t startingindex, size_t lastindex) const {
-}
-
+void Hologram::print(size_t startingindex, size_t lastindex) const {}
--- src/SignalLog.cpp
+++ src/SignalLog.cpp
@@ -1,3 +1,4 @@
+#include "HolomineLib/Dataframe.hpp"
 #include <cstddef>
 #include <ctime>
 #include <fstream>
@@ -6,42 +7,50 @@
 #include <limits>
 #include <sstream>
 #include <stdexcept>
-#include <vector>
 #include <string>
-#include "HolomineLib/Dataframe.hpp"
+#include <vector>
 
-SignalLog::SignalLog(const std::string& filename, const long long& firstTimestamp, const long long& lastTimestamp) {
+SignalLog::SignalLog(const std::string &filename,
+                     const long long &firstTimestamp,
+                     const long long &lastTimestamp) {
   std::ifstream file(filename);
-  
+
   if (!file.is_open()) {
-      throw std::invalid_argument("failed to open file: " + filename);
+    throw std::invalid_argument("failed to open file: " + filename);
   }
+
+  if (file.peek() == EOF) {
+    setSize(0);
+    return;
+  }
+
+  //["DATE", "FREQUENCY_TX", "FREQUENCY_RX", "MOD_I", "PHASE_I", "MOD_Q",
+  //"PHASE_Q", "MOD", "PHASE"];
   std::string line;
-
-  //["DATE", "FREQUENCY_TX", "FREQUENCY_RX", "MOD_I", "PHASE_I", "MOD_Q", "PHASE_Q", "MOD", "PHASE"];
-  if (getline(file, line))
-
-  while (getline(file, line)) {
-    std::stringstream ss(line);
-    std::string cell;
-    signalDatapoint datapoint;
+  if (getline(file, line)) {
+    while (getline(file, line)) {
+      std::stringstream ss(line);
+      std::string cell;
+      signalDatapoint datapoint;
 
       getline(ss, cell, ' ');
       tm t = {};
       std::istringstream date_ss(cell);
-      
-      date_ss >> std::get_time(&t, "%Y/%m/%d-%H:%M:%S"); 
-      
+
+      date_ss >> std::get_time(&t, "%Y/%m/%d-%H:%M:%S");
+
       char dot;
       int micros = 0;
       date_ss >> dot >> micros;
 
       time_t utc_seconds = timegm(&t);
 
-      datapoint.timestamp = (static_cast<long long>(utc_seconds) * 1000000) + micros;
+      datapoint.timestamp =
+          (static_cast<long long>(utc_seconds) * 1000000) + micros;
 
-      if (datapoint.timestamp >= firstTimestamp && datapoint.timestamp <= lastTimestamp) {
-        for(int i=0; i<6; ++i) {
+      if (datapoint.timestamp >= firstTimestamp &&
+          datapoint.timestamp <= lastTimestamp) {
+        for (int i = 0; i < 6; ++i) {
           getline(ss, cell, ' ');
         }
 
@@ -55,88 +64,78 @@ SignalLog::SignalLog(const std::string& filename, const long long& firstTimestam
         datapoint.y = 0;
 
         data.push_back(datapoint);
-        setSize(getSize() + 1);
       }
+    }
   }
+  setSize(data.size());
 };
 
 void SignalLog::print(size_t startingIndex, size_t lastIndex) const {
   for (size_t i = startingIndex; i < lastIndex; i++) {
-    std::cout << data[i].timestamp << " " << data[i].x << " " << data[i].y << " " << data[i].Mod << " " << data[i].Phase << "\n"; 
+    std::cout << data[i].timestamp << " " << data[i].x << " " << data[i].y
+              << " " << data[i].Mod << " " << data[i].Phase << "\n";
   }
 }
 
-double intep1d(double& x1, double& x2, long long& y1, long long& y2, long long& yNew) {
-  // (x_2 - x_1)
-  double op1 = x2 - x1; 
-  // (date_new - date_1)
-  double op2 = yNew - y1;
-  // (date_2-date_1)
-  double op3 = y2 - y1;
+double intep1d(double x1, double x2, long long t1, long long t2,
+               long long tNew) {
   // x_new = x_1 + (x_2 - x_1) * (date_new - date_1)/(date_2-date_1)
-  double signalX = x1 + op1 * (op2 / op3);
-  if (signalX < 0) {
-  }
-
-  return signalX;
+  double div = static_cast<double>(tNew - t1) / (t2 - t1);
+  return x1 + (x2 - x1) * div;
 }
 
-void SignalLog::mapOver(const PositionLog& position) {
-  std::vector<positionDatapoint> positionData = position.getData();
-  int startingIndex = 0;
+void SignalLog::mapOver(const PositionLog &position) {
+  const std::vector<positionDatapoint> &positionData = position.getData();
+  if (positionData.empty()) {
+    std::cerr << "Can't map over an empty vector" << std::endl;
+    return;
+  } else if (data.empty()) {
+    std::cerr << "Can't map an empty vector" << std::endl;
+  }
+  size_t startingIndex = 0;
+
   double minX = std::numeric_limits<double>::max();
   double minY = std::numeric_limits<double>::max();
   double width = std::numeric_limits<double>::min();
-  double length = std::numeric_limits<double>::min();;
-  for (std::size_t i = 0; i < size ; i++) {
-    for (std::size_t j = startingIndex; j < position.getSize(); j++) {
-      if ( positionData[j].timestamp == data[i].timestamp ) {
-        setX(i, positionData[j].x);
-        setY(i, positionData[j].y);
+  double length = std::numeric_limits<double>::min();
 
-        if (positionData[j].x > width)
-          width = positionData[j].x;
-        if (positionData[j].y > length)
-          length = positionData[j].y;
-        if (positionData[j].x < minX)
-          minX = positionData[j].x;
-        if (positionData[j].y < minY)
-          minY = positionData[j].y;
+  for (std::size_t i = 0; i < size; i++) {
+    for (std::size_t j = startingIndex; j < positionData.size(); j++) {
+      if (positionData[j].timestamp == data[i].timestamp) {
+        data[i].x = positionData[j].x;
+        data[i].y = positionData[j].y;
 
-        setWidth(width);
-        setLength(length);
-        setMinX(minX);
-        setMinY(minY);
+        if (data[i].x > width) width = data[i].x;
+        if (data[i].y > length) length = data[i].y;
+        if (data[i].x < minX) minX = data[i].x;
+        if (data[i].y < minY) minY = data[i].y;
 
         startingIndex = j;
         break;
 
-      } else if (position.getData()[j].timestamp > data[i].timestamp) {
-        double signalX = intep1d(positionData[j-1].x, positionData[j].x, positionData[j-1].timestamp, positionData[j].timestamp, data[i].timestamp);
-        double signalY = intep1d(positionData[j-1].y, positionData[j].y, positionData[j-1].timestamp, positionData[j].timestamp, data[i].timestamp);
+      } else if (positionData[j].timestamp > data[i].timestamp) {
+        double signalX = intep1d(positionData[j - 1].x, positionData[j].x,
+                                 positionData[j - 1].timestamp,
+                                 positionData[j].timestamp, data[i].timestamp);
+        double signalY = intep1d(positionData[j - 1].y, positionData[j].y,
+                                 positionData[j - 1].timestamp,
+                                 positionData[j].timestamp, data[i].timestamp);
 
-        setX(i, signalX);
-        setY(i, signalY);
+        data[i].x = signalX;
+        data[i].y = signalY;
 
-        if (signalX > width)
-          width = signalX;
-        if (signalY > length)
-          length = signalY;
-        if (signalX < minX)
-          minX = signalX;
-        if (signalY < minY)
-          minY = signalY;
-        
-        setWidth(width);
-        setLength(length);
-        setMinX(minX);
-        setMinY(minY);
+        if (signalX > width) width = signalX;
+        if (signalY > length) length = signalY;
+        if (signalX < minX) minX = signalX;
+        if (signalY < minY) minY = signalY;
 
-        startingIndex = j;
+        startingIndex = j - 1;
         break;
       }
-      startingIndex = j;
-      
     }
   }
+  setWidth(width);
+  setLength(length);
+  setMinX(minX);
+  setMinY(minY);
 }
